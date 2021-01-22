fde6a2dfe3e71fd15c67d7192ac0a721a4c2b395
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2021-01-21 17:02:43 -0800

    added initial code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +16,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Cut N Edge</h1>
+      <h3>Services:</h3>
+      <p>Service 1</p>
+      <p>Service 2</p>
+      <p>Service 3</p>
     </div>
   );
 }
