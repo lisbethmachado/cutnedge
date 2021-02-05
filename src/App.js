881104056c7c4e8175036ae1c50cac9b966efdaf
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2021-02-05 12:32:00 -0800

    added deploy script and homepage in package json

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1>Cut N Edge</h1>
-      <h3>Services:</h3>
-      <p>Service 1</p>
-      <p>Service 2</p>
-      <p>Service 3</p>
+      <h1>Hello</h1>
     </div>
   );
 }
